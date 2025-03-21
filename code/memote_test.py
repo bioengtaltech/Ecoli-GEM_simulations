af2c921939a03f3d4b39fe7d5dae4f2fa779e763
KennetLindquist <kennetl@student.chalmers.se>
2025-03-21 11:47:42 +0100

    feat(memote): add YML file support and utility functions
    feat(memote): add YML file support and utility functions.
    - Update `memote_test.py` to enable YML file usage
    - Introduce `memote_test_utils.py` with helper functions for Memote test YML file usage

--- code/memote_test.py
+++ code/memote_test.py
@@ -1,15 +1,34 @@
-import memote.suite.api as memote_api
+import logging
+import memote_test_utils
 import cobra
+import memote.suite.api as memote_api
 
-# Read model
-model = cobra.io.read_sbml_model('model/iML1515.xml')
+logging.basicConfig(level=logging.INFO, format="%(message)s")
 
+# Define model directory
+model_directory = "model/"
+
+# Find model file. Requires either XML or YML file (prioritize XML)
+model_path = memote_test_utils.find_model_file_path(model_directory)
+
+# Load model (if YML, genes.tsv, mets.tsv, and rxns.tsv files are required)
+if model_path.endswith(".xml"):
+    model = cobra.io.read_sbml_model(model_path)
+    logging.info(f"Loaded XML model: {model_path}")
+elif model_path.endswith(".yml"):
+    model = cobra.io.load_yaml_model(model_path)
+    logging.info(f"Loaded YML model: {model_path}")
+
+    # Load annotation files
+    annotation_data = memote_test_utils.load_annotation_tsv_files(model_directory)
+    # Append annotation information to model
+    memote_test_utils.append_annotations(model, annotation_data["genes.tsv"], annotation_data["mets.tsv"], annotation_data["rxns.tsv"])
+    
 # Run memote test
 _, result = memote_api.test_model(model, exclusive=None, skip=None, results=True)
 
 # Generate html report
 html_report = memote_api.snapshot_report(result)
-
 
 # Save html report
 report_path = 'memote_report.html'
