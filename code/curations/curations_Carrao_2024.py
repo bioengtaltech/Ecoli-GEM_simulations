d84e957ab7c744b8cd78f39cb17763d5505fbe95
KennetLindquist <kennetl@student.chalmers.se>
2025-06-12 16:24:30 +0200

    feat: GLYCK removal investigation
    feat: GLYCK removal investigation on impact on related metabolites. Conclusion: No dead-end reactions produced -> keep all metabolites involved in GLYCK

--- code/curations/curations_Carrao_2024.py
+++ code/curations/curations_Carrao_2024.py
@@ -15,7 +15,38 @@ print(model.reactions.THD2pp.reaction)
 print(model.reactions.GLYCK2.gene_reaction_rule)
 model.reactions.GLYCK2.gene_reaction_rule = '(b3124 or b0514)'
 print(model.reactions.GLYCK2.gene_reaction_rule)
+
+# Metabolites of GLYCK
+metabolites = [met.id for met in model.reactions.GLYCK.metabolites]
+
+# remove GLYCK
+print(model.reactions.GLYCK.reaction)
+print(model.reactions.GLYCK2.reaction)
 model.remove_reactions(['GLYCK'])
+
+# Check other reactions for GLYCK metabolites
+for met_id in metabolites:
+    met = model.metabolites.get_by_id(met_id)
+    print(f"\nMetabolite: {met.id} ({met.name})")
+    for rxn in met.reactions:
+        print(f" - {rxn.id}: {rxn.reaction}")
+
+def orphan_check(met_id):
+    met = model.metabolites.get_by_id(met_id)
+    producing = [r for r in met.reactions if met in r.products]
+    consuming = [r for r in met.reactions if met in r.reactants]
+    print(f"\n{met_id} -> Produced by: {len(producing)} reactions, Consumed by: {len(consuming)} reactions")
+
+for met_id in metabolites:
+    orphan_check(met_id)
+
+# Metabolite results
+# 3pg_c -> Produced by: 2 reactions, Consumed by: 2 reactions
+# adp_c -> Produced by: 279 reactions, Consumed by: 8 reactions
+# atp_c -> Produced by: 4 reactions, Consumed by: 345 reactions
+# glyc__R_c -> Produced by: 5 reactions, Consumed by: 2 reactions
+# h_c -> Produced by: 786 reactions, Consumed by: 293 reactions
+# Conclusion: No dead-end reactions produced, keep all metabolites
 
 # HSDy 
 print(model.reactions.HSDy.bounds)
